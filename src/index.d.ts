139e8b1ae15099d5b336dd3af3e74212f903eb63
James Teow <james.teow83@gmail.com>
2020-11-20 08:57:50 -0500

    Declare exports

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,2 +1,2 @@
-export function toTitleCase(input: string, useRules: boolean=false): string;
-export function isString(input: any): boolean;
+export declare function toTitleCase(input: string, useRules: boolean=false): string;
+export declare function isString(input: any): boolean;
