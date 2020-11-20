a88e1df0fbda45fb384ffe9a1d740626eb0930e6
James Teow <james.teow83@gmail.com>
2020-11-20 02:27:58 -0500

    Change to export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function isString(input){
+export function isString(input){
   return typeof input === 'string' || input instanceof String
 }
 
@@ -6,7 +6,7 @@ function titleCaseLetter(letter){
   return letter.toUpperCase();
 }
 
-function toTitleCase(input, useRules = false){
+export function toTitleCase(input, useRules = false){
   if (!isString(input)) return input;
   else if (useRules){
     return input.replace(/((?:^|\.)\W|\b(?!(?:a|at|by|in|of|out|on|en|re|vs|bar|mid|per|pro|qua|til|amid|anti|atop|down|less|like|near|over|up|to|but|for|off|out|via|other|from|ll|ve|re|into|unto|with|for|and|nor|but|or|v|yet|so|t|s)\b)\w)/g, 
@@ -17,6 +17,3 @@ function toTitleCase(input, useRules = false){
   }
 }
 
-module.exports = {
-  toTitleCase, isString
-};
