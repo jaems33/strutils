75aea2a1256b828aaaafe5eb23e8dc4bb4719da7
James Teow <james.teow83@gmail.com>
2020-11-20 09:02:52 -0500

    Add functions to a module.

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,2 +1,4 @@
-export declare function toTitleCase(input: string, useRules: boolean=false): string;
-export declare function isString(input: any): boolean;
+export module Strutils {
+  export declare function toTitleCase(input: string, useRules: boolean=false): string;
+  export declare function isString(input: any): boolean;
+}
