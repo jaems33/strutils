b961e236cd47ba94a4ed4e060b18aa1a5a96ccf2
James Teow <james.teow83@gmail.com>
2020-11-20 01:49:55 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+function isString(input){
+  return typeof input === 'string' || input instanceof String
+}
+
+function titleCaseLetter(letter){
+  return letter.toUpperCase();
+}
+
+function toTitleCase(input, useRules = false){
+  if (!isString(input)) return input;
+  else if (useRules){
+    return input.replace(/((?:^|\.)\W|\b(?!(?:a|at|by|in|of|out|on|en|re|vs|bar|mid|per|pro|qua|til|amid|anti|atop|down|less|like|near|over|up|to|but|for|off|out|via|other|from|ll|ve|re|into|unto|with|for|and|nor|but|or|v|yet|so|t|s)\b)\w)/g, 
+    titleCaseLetter);
+  } else {
+    return input.replace(/((:^)\w|\b)\w/g, 
+    titleCaseLetter);
+  }
+}
+
+module.exports = {
+  toTitleCase, isString
+};
