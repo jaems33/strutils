775ba88013027dce8a71cf2e59c8175e270b00a6
James Teow <james.teow83@gmail.com>
2020-11-20 09:16:28 -0500

    Modularize

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,2 @@
-export module Strutils {
-  export declare function toTitleCase(input: string, useRules: boolean=false): string;
-  export declare function isString(input: any): boolean;
-}
+export declare function toTitleCase(input: string, useRules: boolean=false): string;
+export declare function isString(input: any): boolean;
